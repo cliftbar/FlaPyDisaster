ff9ad0e700b8f9b2fc10e18308f9cd82270ca363
cliftbar <cwbarclift@gmail.com>
2017-12-10 20:10:18 -0800

    updating docker integration

--- FlaPyDisaster/docker/runFlaPyDisaster.sh
+++ FlaPyDisaster/docker/runFlaPyDisaster.sh
@@ -1,2 +1,1 @@
-mkdir -p hurricaneEvents
-docker run -p 5555:5555 -u $(id -u):10$(id -g) -v $(pwd)/hurricaneEvents:/FlaPySrc/FlaPyDisaster/FlaPyDisaster/users/events/hurricane -it flapydisaster
+docker run -p 5555:5555 -u $(id -u):$(id -g) -v $(pwd)/../users:/home/flapydisaster/FlaPyDisaster/FlaPyDisaster/users -it --rm flapydisaster
